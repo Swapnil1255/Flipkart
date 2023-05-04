9383e90a802e17fc2c30a4dd6881be7c24be5459
Swapnil1255 <swapnilkulkarni1311@gmail.com>
2023-05-04 17:20:52 +0530

    first commit

---
+++ flipkart.js
@@ -0,0 +1,43 @@
+function show_block(x) {
+    x.style.display = 'block';
+
+}
+function hide_block(x) {
+    x.style.display = 'none';
+    x.style.color = 'red';
+}
+function show(y){
+y.style.display="block";
+}
+function slide_left(x){
+    x.style.transform = 'translateX(-614px)';
+}
+function slide_right(x){
+    x.style.transform = 'translateX(0px)';
+}
+function hide(x){
+    x.style.display="none";
+}
+
+function changeMode(){
+        let mybody = document.body;
+        mybody.classList.toggle('mydark');
+        let b1=document.getElementsByClassName('x2');
+        b1.classList.toggle('mydark1')
+}
+
+
+
+ 
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    // document.body.style.opacity = '0.7'
+    document.querySelector("body").style.overflow='hidden';
+}
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('main').style.opacity = '1'
+    document.querySelector("body").style.overflow='visible';
+}
+
